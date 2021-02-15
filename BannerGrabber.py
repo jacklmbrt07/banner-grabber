4dc4b1c781dd2323d67a111e86224f67daacc6c5
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-15 11:42:28 -0800

    idk why it doesnt work

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -1,3 +1,25 @@
+# import socket
+
+# s = socket.socket()
+
+# ip = input("Please enter the IP: ")
+# port = str(input("Please enter the port: "))
+
+# s.connect((ip, int(port)))
+
+# print(s.recv(1024))
+
 import socket
 
-s = socket.socket
+
+def banner(ip, port):
+    s = socket.socket()
+    s.connect((ip, int(port)))
+    print(s.recv(1024))    
+
+def main():
+    ip = input("Please enter the IP: ")
+    port = str(input("Please enter the Port: "))
+    banner(ip, int(port))
+
+main()
