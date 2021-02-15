ca6b90c688305ca4342a194b688190e9f0f013a7
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-15 11:47:02 -0800

    ok

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -15,7 +15,7 @@ import socket
 def banner(ip, port):
     s = socket.socket()
     s.connect((ip, int(port)))
-    print(s.recv(1024))    
+    print(str(s.recv(1024))).strip('b')    
 
 def main():
     ip = input("Please enter the IP: ")
